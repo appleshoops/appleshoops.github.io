2f75777d99b0bc198638dcb3bb4f09d4960c1eb4
AppleSheep <95598768+appleshoops@users.noreply.github.com>
2024-06-24 21:48:25 +1000

    Fuck needle fetch is cooler

--- twitter.js
+++ twitter.js
@@ -1,17 +1,18 @@
-const needle = require('needle');
-
 const UserID = "1560203265903833089"
-const tweetURL = `https://api.twitter.com/2/users/${UserID}/tweets`;
+const proxyURL = 'https://corsproxy.io/?' + encodeURIComponent(`https://api.twitter.com/2/users/${UserID}/tweets`);
+const tweetURL = new URL(proxyURL);
 
-const BearerToken = process.env.BearerToken;
+const BearerToken = 'AAAAAAAAAAAAAAAAAAAAAJDUuQEAAAAAPUBLv2XYL32gzyqmONwebGEuIwc%3DkcZ5zslsHHKVS0Vuo2QCcoJC7y76U8ARDA3cByP8U26XTdOBmY';
 
 const getUserTweets = async () => {
     let userTweets = [];
     let params = {
         "max_results": 15,
         "tweet.fields": "created_at",
-        "expansions": "author_id"
+        "expansions": "author_id",
     }
+
+    Object.keys(params).forEach(key => tweetURL.searchParams.append(key, params[key]));
 
     const options = {
         headers: {
@@ -25,19 +26,19 @@ const getUserTweets = async () => {
     console.log("Retrieving Tweets...")
 
     try {
-        const response = await needle('get', tweetURL, params, options);
-        if (response.statusCode === 200) {
-            userTweets = (response.body);
-            console.log("WE GOT THE TWEETS WOOOOOO")
+        const response = await fetch(tweetURL, options);
+        if (response.ok) {
+            const userTweets = await response.json();
+            console.log("WE GOT TWEETS!!!");
+            return userTweets;
         } else {
-            userTweets = "No Tweets Or Something"
-            console.error("No Twwets :(", response.body, response.statusCode)
+            console.error("We have an error: ", await response.text(), response.status);
+            return "No Tweets :("
         }
     } catch (error) {
-        console.error("Error:", error)
+        console.error("Error:", error);
+        return null;
     }
-
-    return userTweets;
 }
 
 getUserTweets().then(tweets => console.log(tweets));
