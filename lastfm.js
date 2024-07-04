b718fda7f0765e4418521429fce6db836a2b746e
AppleSheep <95598768+appleshoops@users.noreply.github.com>
2024-07-04 13:39:03 +1000

    jhk=3wg

--- lastfm.js
+++ lastfm.js
@@ -4,6 +4,7 @@ const sharedSecret = '79b945f9ad2541bd64c67132cc888918'
 const parser = new DOMParser();
 
 async function getUserData() {
+    let scrobbles = 0;
     const userURL = `http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=AppleSheep&api_key=${apiKey}&format=json`;
     try {
         const response = await fetch(userURL);
@@ -20,6 +21,7 @@ async function getUserData() {
     } catch (error) {
         console.error(error.message);
     }
+    document.getElementById("scrobblesText").textContent = `${scrobbles} scrobbles`;
 }
 
 async function getTopArtists() {
@@ -42,36 +44,35 @@ async function getTopArtists() {
         let iTunesArtistImages = {}; // Object to store the URLs
 
         for (let i = 0; i < artistJSON.topartists.artist.length; i++) {
-        const artist = artistJSON.topartists.artist[i];
-        iTunesSearchURL = 'https://corsproxy.io/?' + encodeURIComponent(`https://itunes.apple.com/search?term=${artist.name}&country=AU&entity=musicArtist&attribute=artistTerm&limit=1&lang=en_AU`)
-        try {
-            const response = await fetch(iTunesSearchURL);
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-         }
-            const data = await response.json();
-            iTunesArtistURL = 'https://corsproxy.io/?' + encodeURIComponent(data.results[0].artistLinkUrl);
-            iTunesArtistsData[`iTunesArtist${i + 1}`] = data;
-
+            const artist = artistJSON.topartists.artist[i];
+            iTunesSearchURL = 'https://corsproxy.io/?' + encodeURIComponent(`https://itunes.apple.com/search?term=${artist.name}&country=AU&entity=musicArtist&attribute=artistTerm&limit=1&lang=en_AU`)
             try {
-                const response = await fetch(iTunesArtistURL);
+                const response = await fetch(iTunesSearchURL);
                 if (!response.ok) {
                     throw new Error(`Error: ${response.status}`);
-                }
-                const iTunesArtistPage = await response.text();
-                const iTunesArtistDoc = parser.parseFromString(iTunesArtistPage, 'text/html');
-                imageURL = iTunesArtistDoc.querySelector('meta[property="og:image"]').getAttribute('content');
-                if (imageURL.endsWith("cw.png")) {
-                    imageURL = imageURL.replace("cw.png", "cc.png");
-                    console.log(imageURL);
-                }
-                iTunesArtistImages[`iTunesArtist${i + 1}`] = imageURL
-            } catch (error) {
-                console.error(error.message);
             }
-        } catch (error) {
-        console.error(error.message);
-    }
+                const data = await response.json();
+                iTunesArtistURL = 'https://corsproxy.io/?' + encodeURIComponent(data.results[0].artistLinkUrl);
+                iTunesArtistsData[`iTunesArtist${i + 1}`] = data;
+
+                try {
+                    const response = await fetch(iTunesArtistURL);
+                    if (!response.ok) {
+                        throw new Error(`Error: ${response.status}`);
+                    }
+                    const iTunesArtistPage = await response.text();
+                    const iTunesArtistDoc = parser.parseFromString(iTunesArtistPage, 'text/html');
+                    imageURL = iTunesArtistDoc.querySelector('meta[property="og:image"]').getAttribute('content');
+                    if (imageURL.endsWith("cw.png")) {
+                        imageURL = imageURL.replace("cw.png", "cc.png");
+                    }
+                    iTunesArtistImages[`iTunesArtist${i + 1}`] = imageURL
+                } catch (error) {
+                    console.error(error.message);
+                }
+            } catch (error) {
+            console.error(error.message);
+        }
 }
     } catch(error) {
         console.error(error.message);
@@ -99,6 +100,13 @@ async function getTopAlbums() {
         topAlbumArtist3 = albumJSON.topalbums.album[2].artist.name;
         topAlbumArtist4 = albumJSON.topalbums.album[3].artist.name;
         topAlbumArtist5 = albumJSON.topalbums.album[4].artist.name;
+
+        topAlbumImage1 = albumJSON.topalbums.album[0].image[1]['#text'];
+        topAlbumImage2 = albumJSON.topalbums.album[1].image[1]['#text'];
+        topAlbumImage3 = albumJSON.topalbums.album[2].image[1]['#text'];
+        topAlbumImage4 = albumJSON.topalbums.album[3].image[1]['#text'];
+        topAlbumImage5 = albumJSON.topalbums.album[4].image[1]['#text'];
+
     } catch(error) {
         console.error(error.message);
     }
@@ -132,6 +140,12 @@ async function getRecentTracks() {
         recentTrackArtist3 = tracksJSON.recenttracks.track[2].artist['#text'];
         recentTrackArtist4 = tracksJSON.recenttracks.track[3].artist['#text'];
         recentTrackArtist5 = tracksJSON.recenttracks.track[4].artist['#text'];
+
+        recentTrackImage1 = tracksJSON.recenttracks.track[0].image[1]['#text'];
+        recentTrackImage2 = tracksJSON.recenttracks.track[1].image[1]['#text'];
+        recentTrackImage3 = tracksJSON.recenttracks.track[2].image[1]['#text'];
+        recentTrackImage4 = tracksJSON.recenttracks.track[3].image[1]['#text'];
+        recentTrackImage5 = tracksJSON.recenttracks.track[4].image[1]['#text'];
     } catch(error) {
         console.error(error.message);
     }
@@ -141,3 +155,5 @@ getUserData();
 getTopArtists();
 getTopAlbums();
 getRecentTracks();
+
+document.addEventListener('DOMContentLoaded', getUserData);
